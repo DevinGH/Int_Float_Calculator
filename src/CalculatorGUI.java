47a47a3c7fd463db789c987ed41bc64a7eed0cf2
Devin <devinhanson28@gmail.com>
2022-03-10 15:22:17 +0900

    Added Buttons and fixed some spacing issues.

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -1,26 +1,31 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 
 public class CalculatorGUI {
-    public CalculatorGUI(int defaultWidth, int defaultHeight) {
+    public CalculatorGUI(int width, int height) {
         JFrame frame = new JFrame("Calculator");
 
-        frame.setSize(defaultWidth, defaultHeight);
+        frame.setSize(width, height);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainPanel = new JPanel(new BorderLayout());
 
         JPanel northPanel2 = new JPanel(new FlowLayout());
-        JPanel centerPanel2 = new JPanel(new GridLayout(4, 4));
+        JPanel centerPanel2 = new JPanel(new GridLayout(4, 4, 10, 20));
+
         JPanel southPanel2 = new JPanel(new BorderLayout());
 
-        northPanel2.setSize(300, 100);
         northPanel2.setBackground(Color.black);
-        centerPanel2.setSize(300, 300);
+        northPanel2.setBorder(new EmptyBorder(35, 5, 5, 5));
+        northPanel2.setPreferredSize(new Dimension(width, 100));
         centerPanel2.setBackground(Color.BLUE);
-        southPanel2.setSize(300, 100);
+        centerPanel2.setPreferredSize(new Dimension(width, 300));
         southPanel2.setBackground(Color.CYAN);
+        southPanel2.setPreferredSize(new Dimension(width, 100));
+
 
         JPanel southEastPanel3 = new JPanel(new FlowLayout());
         JPanel southWestPanel3 = new JPanel(new BorderLayout());
@@ -30,6 +35,54 @@ public class CalculatorGUI {
 
         JPanel swsWestPanel5 = new JPanel(new FlowLayout());
         JPanel swsEastPanel5 = new JPanel(new FlowLayout());
+
+        /*
+        Creating Components
+         */
+        JTextField display = new JTextField(50);
+        display.setPreferredSize(new Dimension(100, 40));
+
+        JRadioButton calcPower = new JRadioButton();
+        JButton button1 = new JButton();
+        JButton button2 = new JButton();
+        JButton button3 = new JButton();
+        JButton button4 = new JButton();
+        JButton button5 = new JButton();
+        JButton button6 = new JButton();
+        JButton button7 = new JButton();
+        JButton button8 = new JButton();
+        JButton button9 = new JButton();
+        JButton button10 = new JButton();
+        JButton button11 = new JButton();
+        JButton button12 = new JButton();
+        JButton button13 = new JButton();
+        JButton button14 = new JButton();
+        JButton button15 = new JButton();
+
+
+
+        /*
+        Adding components to panels
+         */
+        northPanel2.add(display);
+
+        centerPanel2.add(calcPower);
+        centerPanel2.add(button1);
+        centerPanel2.add(button2);
+        centerPanel2.add(button3);
+        centerPanel2.add(button4);
+        centerPanel2.add(button5);
+        centerPanel2.add(button6);
+        centerPanel2.add(button7);
+        centerPanel2.add(button8);
+        centerPanel2.add(button9);
+        centerPanel2.add(button10);
+        centerPanel2.add(button11);
+        centerPanel2.add(button12);
+        centerPanel2.add(button13);
+        centerPanel2.add(button14);
+        centerPanel2.add(button15);
+
 
         /*
         Adding Panels and Sub-panels to frame
