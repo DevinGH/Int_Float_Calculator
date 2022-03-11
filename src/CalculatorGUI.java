d3420eaddaa7369e3a3e7b4ac8a28e1a4a22e3a5
Devin <devinhanson28@gmail.com>
2022-03-12 03:51:01 +0900

    Severely changed the code to include way more shorter methods for cleaner code and added more comments.

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -1,10 +1,9 @@
 /**
- * Imports
+ * Group Members: Brianna Andres, Thomas Tien, Devin Hanson
  */
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import java.util.*;
 
 /**
  * Class that sets up basic Calculator GUI
@@ -38,74 +37,58 @@ public class CalculatorGUI {
     public static void GUIInit(){
         CalculatorGUI calc = new CalculatorGUI(300, 500);
 
-        /*
-        Creating Panels and setting up properties
-         */
         JPanel mainPanel = new JPanel(new BorderLayout());
 
+        mainPanel.add(northPanel(), BorderLayout.NORTH);
+        mainPanel.add(centerPanel(), BorderLayout.CENTER);
+        mainPanel.add(southPanel(), BorderLayout.SOUTH);
+
+        frame.add(mainPanel);
+        frame.pack();
+    }
+
+    /**
+     * Creates the North Panel containing the text display
+     * @return
+     */
+    public static JPanel northPanel(){
+        /*
+        Creating North Panel
+         */
         JPanel northPanel2 = new JPanel(new FlowLayout());
-        JPanel centerPanel2 = new JPanel(new GridLayout(4, 4, 10, 25));
-        JPanel southPanel2 = new JPanel(new BorderLayout());
 
         northPanel2.setBackground(Color.black);
         northPanel2.setBorder(new EmptyBorder(35, 20, 10, 20));
         northPanel2.setPreferredSize(new Dimension(width, 100));
 
-        centerPanel2.setBackground(Color.BLACK);
-        centerPanel2.setPreferredSize(new Dimension(width, 200));
-        centerPanel2.setBorder(new EmptyBorder(5, 5, 10, 5));
-
-        southPanel2.setBackground(Color.BLACK);
-        southPanel2.setPreferredSize(new Dimension(width, 100));
-
-        JPanel radioPanel = new JPanel(new FlowLayout());
-        radioPanel.setBackground(Color.BLACK);
-        radioPanel.setPreferredSize(new Dimension(40, 40));
-
-        JPanel southEastPanel3 = new JPanel(new FlowLayout());
-
-        southEastPanel3.setPreferredSize(new Dimension(70, 90));
-        southEastPanel3.setBackground(Color.BLACK);
-
-        JPanel southWestPanel3 = new JPanel(new BorderLayout());
-
-        southWestPanel3.setPreferredSize(new Dimension(230, 90));
-        southWestPanel3.setBackground(Color.BLACK);
-
-        JPanel swNorthPanel4 = new JPanel(new GridLayout(1, 3, 10, 5));
-
-        swNorthPanel4.setBackground(Color.BLACK);
-        swNorthPanel4.setPreferredSize(new Dimension(230, 50));
-        swNorthPanel4.setBorder(new EmptyBorder(5, 5, 5, 5));
-
-        JPanel swSouthPanel4 = new JPanel(new BorderLayout());
-
-        swSouthPanel4.setBackground(Color.BLACK);
-        swSouthPanel4.setPreferredSize(new Dimension(230, 50));
-        swSouthPanel4.setBorder(new EmptyBorder(0, 5, 5, 5));
-
-        JPanel swsWestPanel5 = new JPanel(new FlowLayout());
-
-        swsWestPanel5.setPreferredSize(new Dimension(145, 50));
-        swsWestPanel5.setBackground(Color.BLACK);
-
-        JPanel swsEastPanel5 = new JPanel(new FlowLayout());
-
-        swsEastPanel5.setPreferredSize(new Dimension(75, 50));
-        swsEastPanel5.setBackground(Color.BLACK);
-
-
         /*
-        Creating Components
-         */
-        /*
-        Components for North panel
+        Components for panel
          */
         JTextField display = new JTextField(25);
         display.setPreferredSize(new Dimension(100, 40));
 
         /*
-        Components for Central panel
+        Adding Components to panel
+         */
+        northPanel2.add(display);
+
+        return northPanel2;
+    }
+
+    /**
+     * Creates Radio Button Panel in order to contain to Radio buttons on the same block
+     * @return
+     */
+    public static JPanel radioPanel(){
+        /*
+        Creating Radio Panel
+         */
+        JPanel radioPanel = new JPanel(new FlowLayout());
+        radioPanel.setBackground(Color.BLACK);
+        radioPanel.setPreferredSize(new Dimension(40, 40));
+
+         /*
+        Components for panel
          */
         ButtonGroup group = new ButtonGroup();
 
@@ -116,6 +99,35 @@ public class CalculatorGUI {
         calcPowerOFF.setBackground(Color.black);
         calcPowerOFF.setForeground(Color.WHITE);
 
+        group.add(calcPowerOFF);
+        group.add(calcPowerON);
+
+        /*
+        Adding Components to panel
+         */
+        radioPanel.add(calcPowerON);
+        radioPanel.add(calcPowerOFF);
+
+        return radioPanel;
+    }
+
+    /**
+     * Creates the Center Panel containing most of the buttons on the calculator
+     * @return
+     */
+    public static JPanel centerPanel(){
+        /*
+        Creating Center Panel
+         */
+        JPanel centerPanel2 = new JPanel(new GridLayout(4, 4, 10, 25));
+
+        centerPanel2.setBackground(Color.BLACK);
+        centerPanel2.setPreferredSize(new Dimension(width, 200));
+        centerPanel2.setBorder(new EmptyBorder(5, 5, 10, 5));
+
+        /*
+        Components for panel
+         */
         String[] calcOperationArray = {"C", "DEL", "/", "\u221A", "x\u00B2", "1/x", "-",
                 "7", "8", "9", "x", "4", "5", "6", "+"};
 
@@ -132,42 +144,10 @@ public class CalculatorGUI {
         calcButtonArray[10].setBackground(Color.ORANGE);
         calcButtonArray[14].setBackground(Color.ORANGE);
 
-        group.add(calcPowerOFF);
-        group.add(calcPowerON);
-
         /*
-        Components for South panel
+        Adding Components to panel
          */
-        JButton eqButton = new JButton("=");
-        eqButton.setBackground(Color.ORANGE);
-        eqButton.setPreferredSize(new Dimension(60, 90));
-
-        JButton button1 = new JButton("1");
-        JButton button2 = new JButton("2");
-        JButton button3 = new JButton("3");
-
-        JButton zeroButton = new JButton("0");
-
-        zeroButton.setPreferredSize(new Dimension(140, 50));
-
-        JButton decimalButton = new JButton(".");
-
-        decimalButton.setPreferredSize(new Dimension(65, 50));
-
-        /*
-        Adding components to panels
-         */
-        /*
-        North Panel
-         */
-        northPanel2.add(display);
-
-        /*
-        Center panel
-         */
-        radioPanel.add(calcPowerON);
-        radioPanel.add(calcPowerOFF);
-        centerPanel2.add(radioPanel);
+        centerPanel2.add(radioPanel());
 
         for(int i = 0; i < calcButtonArray.length; i++){
             calcButtonArray[i].setText("" + calcOperationArray[i]);
@@ -175,34 +155,189 @@ public class CalculatorGUI {
             centerPanel2.add(calcButtonArray[i]);
         }
 
+        return centerPanel2;
+    }
+
+    /**
+     * Creates the most bottom left of the calculator containing the zero button
+     * @return
+     */
+    public static JPanel swsWestPanel(){
         /*
-        South Panel
+        Creating South West South West Panel
          */
-        southEastPanel3.add(eqButton);
+        JPanel swsWestPanel5 = new JPanel(new FlowLayout());
+
+        swsWestPanel5.setPreferredSize(new Dimension(145, 50));
+        swsWestPanel5.setBackground(Color.BLACK);
+
+        /*
+        Components for panel
+         */
+        JButton zeroButton = new JButton("0");
+
+        zeroButton.setPreferredSize(new Dimension(140, 50));
+
+        /*
+        Adding Components to panel
+         */
+        swsWestPanel5.add(zeroButton);
+
+        return swsWestPanel5;
+    }
+
+    /**
+     * Creates the right of the bottom left of the calculator containing the decimal button
+     * @return
+     */
+    public static JPanel swsEastPanel(){
+        /*
+        Creating South West South East Panel
+         */
+        JPanel swsEastPanel5 = new JPanel(new FlowLayout());
+
+        swsEastPanel5.setPreferredSize(new Dimension(75, 50));
+        swsEastPanel5.setBackground(Color.BLACK);
+
+        /*
+        Components for panel
+         */
+        JButton decimalButton = new JButton(".");
+
+        decimalButton.setPreferredSize(new Dimension(65, 50));
+
+        /*
+        Adding Components to panel
+         */
+        swsEastPanel5.add(decimalButton);
+
+        return swsEastPanel5;
+    }
+
+    /**
+     * Creates the entire bottom left of the calculator
+     * @return
+     */
+    public static JPanel swSouthPanel(){
+        /*
+        Creating South West South Panel
+         */
+        JPanel swSouthPanel4 = new JPanel(new BorderLayout());
+
+        swSouthPanel4.setBackground(Color.BLACK);
+        swSouthPanel4.setPreferredSize(new Dimension(230, 50));
+        swSouthPanel4.setBorder(new EmptyBorder(0, 5, 5, 5));
+
+        /*
+        Adding Components to panel
+         */
+        swSouthPanel4.add(swsWestPanel(), BorderLayout.WEST);
+        swSouthPanel4.add(swsEastPanel(), BorderLayout.EAST);
+
+        return swSouthPanel4;
+    }
+
+    /**
+     * Creates the top of the bottom left of the calculator containing the 1, 2, and 3 buttons
+     * @return
+     */
+    public static JPanel swNorthPanel(){
+        /*
+        Creating South West North Panel
+         */
+        JPanel swNorthPanel4 = new JPanel(new GridLayout(1, 3, 10, 5));
+
+        swNorthPanel4.setBackground(Color.BLACK);
+        swNorthPanel4.setPreferredSize(new Dimension(230, 50));
+        swNorthPanel4.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+        /*
+        Components for panel
+         */
+        JButton button1 = new JButton("1");
+        JButton button2 = new JButton("2");
+        JButton button3 = new JButton("3");
+
+        /*
+        Adding Components to panel
+        */
         swNorthPanel4.add(button1);
         swNorthPanel4.add(button2);
         swNorthPanel4.add(button3);
-        swsWestPanel5.add(zeroButton);
-        swsEastPanel5.add(decimalButton);
+
+        return swNorthPanel4;
+    }
+
+    /**
+     * Creates the left of the bottom third of the calculator
+     * @return
+     */
+    public static JPanel southWestPanel(){
+        /*
+        Creating South West Panel
+         */
+        JPanel southWestPanel3 = new JPanel(new BorderLayout());
+
+        southWestPanel3.setPreferredSize(new Dimension(230, 90));
+        southWestPanel3.setBackground(Color.BLACK);
 
         /*
-        Adding Panels and Sub-panels to frame
+        Adding Components to panel
+        */
+        southWestPanel3.add(swNorthPanel(), BorderLayout.NORTH);
+        southWestPanel3.add(swSouthPanel(), BorderLayout.SOUTH);
+
+        return southWestPanel3;
+    }
+
+    /**
+     * Creates the bottom right of the calculator containing the equals button
+     * @return
+     */
+    public static JPanel southEastPanel(){
+        /*
+        Creating South East Panel
          */
-        swSouthPanel4.add(swsWestPanel5, BorderLayout.WEST);
-        swSouthPanel4.add(swsEastPanel5, BorderLayout.EAST);
+        JPanel southEastPanel3 = new JPanel(new FlowLayout());
 
-        southWestPanel3.add(swNorthPanel4, BorderLayout.NORTH);
-        southWestPanel3.add(swSouthPanel4, BorderLayout.SOUTH);
+        southEastPanel3.setPreferredSize(new Dimension(70, 90));
+        southEastPanel3.setBackground(Color.BLACK);
 
-        southPanel2.add(southWestPanel3, BorderLayout.WEST);
-        southPanel2.add(southEastPanel3, BorderLayout.EAST);
+        /*
+        Components for panel
+         */
+        JButton eqButton = new JButton("=");
+        eqButton.setBackground(Color.ORANGE);
+        eqButton.setPreferredSize(new Dimension(60, 90));
 
-        mainPanel.add(northPanel2, BorderLayout.NORTH);
-        mainPanel.add(centerPanel2, BorderLayout.CENTER);
-        mainPanel.add(southPanel2, BorderLayout.SOUTH);
+        /*
+        Adding Components to panel
+        */
+        southEastPanel3.add(eqButton);
 
-        frame.add(mainPanel);
-        frame.pack();
+        return southEastPanel3;
+    }
+
+    /**
+     * Creates the South Panel containing the section of the calculator with the longer buttons
+     * @return
+     */
+    public static JPanel southPanel(){
+        /*
+        Creating South Panel
+         */
+        JPanel southPanel2 = new JPanel(new BorderLayout());
+
+        southPanel2.setBackground(Color.BLACK);
+        southPanel2.setPreferredSize(new Dimension(width, 100));
+
+        /*
+        Adding Components to panel
+        */
+        southPanel2.add(southWestPanel(), BorderLayout.WEST);
+        southPanel2.add(southEastPanel(), BorderLayout.EAST);
+
+        return southPanel2;
     }
 
     /**
